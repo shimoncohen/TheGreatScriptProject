412f06a926e13b5a61d8bb6a1c2cee681b8adc77
shimoncohen <shimoncohen655@gmail.com>
2020-07-06 15:13:49 +0300

    Added: Script for installing chrome.

---
+++ installs/chrome.sh
@@ -0,0 +1,14 @@
+DIRECTORY=$(readlink -f $0)
+
+# Check if script is running as root
+bash ${DIRECTORY%/*}/../validation/checkRootPrivileges.sh
+test $? -eq 0 || exit
+
+# Download latest .deb package
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install chrome
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Remove .deb package
+rm -r google-chrome-stable_current_amd64.deb
