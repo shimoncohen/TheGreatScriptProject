29aab0bb92c8ddb379cecf43f4c6ca71674b9183
shimoncohen <shimoncohen655@gmail.com>
2020-07-06 17:30:38 +0300

    Added: Check if command already exists before install.

--- installs/yarn.sh
+++ installs/yarn.sh
@@ -4,9 +4,13 @@ DIRECTORY=$(readlink -f $0)
 bash ${DIRECTORY%/*}/../validation/checkRootPrivileges.sh
 test $? -eq 0 || exit
 
-# Add yarn repository
+# Check if command exists
 bash ${DIRECTORY%/*}/../validation/checkIfCommandExists.sh yarn
-test $? -eq 1 || (curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list)
+test $? -eq 0 && exit
+
+# Add yarn repository
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 # Update packages
 apt-get update -y
